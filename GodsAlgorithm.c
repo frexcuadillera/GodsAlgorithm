f7446c6ad1bacce7027ed1f49f558ddbc939d525
frexcuadillera <fcuadillera@gmail.com>
2019-02-17 01:04:08 +0800

    Initial commit

---
+++ GodsAlgorithm.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+//blue in front value 1
+//orange in left value 2
+//red in right value 3
+//green in back value 4
+//yellow in top value 5
+//white in bottom value 6
+
+void main(){
+	int blue [3][3] = {{1,1,1},{1,1,1},{1,1,1}};
+	int orange [3][3] = {{2,2,2},{2,2,2},{2,2,2}};
+	int red [3][3] = {{3,3,3},{3,3,3},{3,3,3}};
+	int green [3][3] = {{4,4,4},{4,4,4},{4,4,4}};
+	int yellow [3][3] = {{5,5,5},{5,5,5},{5,5,5}};
+	int white [3][3] = {{6,6,6},{6,6,6},{6,6,6}};
+}
+
+void R(int blue[3][3],);
