84160ea41de314476e33d3322160163a10c5393e
frexcuadillera <fcuadillera@gmail.com>
2019-02-17 03:17:27 +0800

    finished R move, fixed indentation

--- GodsAlgorithm.c
+++ GodsAlgorithm.c
@@ -32,37 +32,37 @@ void main(){
 }
 
 void R(int cube[54]){
- int temp1 = cube[2], temp2 = cube[5], temp3 = cube[8];
- 
- cube[2]  = cube[20];
- cube[5]  = cube[23];
- cube[8]  = cube[26];
- 
- cube[20] = cube[47]; 
- cube[23] = cube[50];
- cube[26] = cube[53];
- 
- cube[47] = cube[42];
- cube[50] = cube[39];
- cube[53] = cube[36];
- 
- cube[36] = temp3;
- cube[39] = temp2;
- cube[42] = temp1;
- 
- int tempA = cube[28], tempB = cube[29];
- 
- cube[28] = cube[30];
- cube[29] = cube[27];
- 
- cube[30] = cube[34];
- cube[27] = cube[33];
- 
- cube[34] = cube[32];
- cube[33] = cube[35];
- 
- cube[32] = tempA;
- cube[35] = tempB;
+	int temp1 = cube[2], temp2 = cube[5], temp3 = cube[8];
+
+	cube[2]  = cube[20];
+	cube[5]  = cube[23];
+	cube[8]  = cube[26];
+
+	cube[20] = cube[47]; 
+	cube[23] = cube[50];
+	cube[26] = cube[53];
+
+	cube[47] = cube[42];
+	cube[50] = cube[39];
+	cube[53] = cube[36];
+
+	cube[36] = temp3;
+	cube[39] = temp2;
+	cube[42] = temp1;
+
+	int tempA = cube[28], tempB = cube[29];
+
+	cube[28] = cube[30];
+	cube[29] = cube[27];
+
+	cube[30] = cube[34];
+	cube[27] = cube[33];
+
+	cube[34] = cube[32];
+	cube[33] = cube[35];
+
+	cube[32] = tempA;
+	cube[35] = tempB;
 }
 
 void RPrime(int cube[54]){
